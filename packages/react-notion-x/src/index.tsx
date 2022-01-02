0e6241fc3b7a5eab66a73b8a9688fd6f525b6974
harajune <harajune@gijutsuya.jp>
2022-01-02 16:38:37 +0900

    add search-dialog to exports

--- packages/react-notion-x/src/index.tsx
+++ packages/react-notion-x/src/index.tsx
@@ -5,6 +5,7 @@ export * from './context'
 // heavier dependencies that the core renderer should not depend on explicitly
 // users may want to dynamically load these dependencies
 export * from './components/code'
+export * from './components/search-dialog'
 export * from './components/collection'
 export * from './components/collection-row'
 export * from './components/page-icon'
